5ba290b063dca8769f809666ddf368c23047af76
MarkoVitkovic <marko.vitkovic@outlook.com>
2020-04-21 00:26:31 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    advice: ''
+  }
+
+  componentDidMount(){
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () =>{
+    axios.get('	https://api.adviceslip.com/advice')
+    .then((res) => {
+      const  {advice} = res.data.slip;
+      this.setState({
+        advice: advice
+      })
+    })
+    .catch((err) => {console.log(err)})
+  }
+
+  render() {
+    const {advice} = this.state;
+    return (
+      <div className="app">
+            <h1>{advice}</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
